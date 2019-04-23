ac515d9785c9918e3758e93e70e0d8a899ad3935
lxt <1932862921@qq.com>
2019-04-23 09:06:48 +0800

    聊天修改

--- renderer-process/chat/chat.js
+++ renderer-process/chat/chat.js
@@ -153,20 +153,7 @@ chat.event = ()=>{
     });
 
     // $('#app-record').click(function(){
-    //   moduleId = 'invitation';
-    //   const link = document.querySelector(`link#${moduleId}[rel="import"]`);
-    //   const template = link.import.querySelector('.modal-template')
-    //   let clone = document.importNode(template.content, true);
-    //   layer.open({
-    //     type: 1,
-    //     title: false,
-    //     skin: false, //加上边框
-    //     area: ['340px', '420px'], //宽高
-    //     closeBtn: 0, //不显示关闭按钮
-    //     shadeClose: true, //开启遮罩关闭
-    //     content: '<div class="invitation-modal"></div>'
-    //   });
-    //   document.querySelector('.invitation-modal').appendChild(clone)
+    //   invitation.invitation.init();
     // });
 
     $('#app-chat-search-group-user-input').bind('input propertychange', function(){
